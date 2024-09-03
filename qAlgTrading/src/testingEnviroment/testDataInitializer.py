0efaea1656a7dfde7f932c762a8bdfb1bed87a23
Pyerdowah <zosiasamosia209@gmail.com>
2024-09-03 23:42:37 +0200

    variable name changes

--- qAlgTrading/src/testingEnviroment/testDataInitializer.py
+++ qAlgTrading/src/testingEnviroment/testDataInitializer.py
@@ -6,28 +6,28 @@ from qAlgTrading.src.algorithms.quantumAlgorithm import QuantumAlgorithm
 from qAlgTrading.src.testingEnviroment.algorithmTester import AlgorithmTester
 from qAlgTrading.src.testingEnviroment.resultsPresenter import ResultPresenter
 
-startDate = '2005-04-02'  # data nieprzypadkowa
-numDays = 14
+start_date = '2005-04-02'  # data nieprzypadkowa
+num_days = 14
 
 wig20 = pd.read_csv('../../../data/wig20/WIG20.csv')
-filteredWig20 = wig20[wig20['Date'] >= startDate].head(numDays)
-wig20Assets = filteredWig20[['Open', 'High', 'Low', 'Close']].values
-wig20AssetsArray = np.array(wig20Assets)
+filtered_wig20 = wig20[wig20['Date'] >= start_date].head(num_days)
+wig20_assets = filtered_wig20[['Open', 'High', 'Low', 'Close']].values
+wig20_assets_array = np.array(wig20_assets)
 
 sp500 = pd.read_csv('../../../data/sp500/^SPX.csv')
-filteredSp500 = sp500[sp500['Date'] >= startDate].head(numDays)
-sp500Assets = filteredSp500[['Open', 'High', 'Low', 'Close']].values
-sp500AssetsArray = np.array(sp500Assets)
+filtered_sp500 = sp500[sp500['Date'] >= start_date].head(num_days)
+sp500_assets = filtered_sp500[['Open', 'High', 'Low', 'Close']].values
+sp500_assets_array = np.array(sp500_assets)
 
-classicAlgorithm = ClassicAlgorithm()
-quantumAlgorithm = QuantumAlgorithm()
+classic_algorithm = ClassicAlgorithm()
+quantum_algorithm = QuantumAlgorithm()
 
 tester = AlgorithmTester()
-tester.perform_test(classicAlgorithm, wig20AssetsArray)
-tester.perform_test(quantumAlgorithm, sp500AssetsArray)
+tester.perform_test(classic_algorithm, wig20_assets_array)
+tester.perform_test(quantum_algorithm, sp500_assets_array)
 
-results = np.array([wig20AssetsArray, sp500AssetsArray])
+results = np.array([wig20_assets_array, sp500_assets_array])
 
-resultPresenter = ResultPresenter()
-resultPresenter.print_results_single_chart(results)
-resultPresenter.print_results_separate_chart(results)
+result_presenter = ResultPresenter()
+result_presenter.print_results_single_chart(results)
+result_presenter.print_results_separate_chart(results)
