a40424407b61320dfbc3190d4f6f49f530db08e5
marek <31738037+MarekBorzyszkowski@users.noreply.github.com>
2024-12-08 14:32:37 +0100

    tests prepared

--- qAlgTrading/run_tests.sh
+++ qAlgTrading/run_tests.sh
@@ -5,10 +5,25 @@ if [ ! -d src/venv ]; then
 fi
 source src/venv/bin/activate
 
-echo "Start of tests"
-src/venv/bin/python3 src/main.py example.json
+echo "Start of tests S&P"
+src/venv/bin/python3 src/main.py jsons/S\&P.json
 echo "End of tests"
-echo "Start of tests"
-src/venv/bin/python3 src/main.py example_2.json
+echo "Start of tests wig_20"
+src/venv/bin/python3 src/main.py jsons/wig_20.json
+echo "End of tests"
+echo "Start of tests CDR"
+src/venv/bin/python3 src/main.py jsons/CDR.json
+echo "End of tests"
+echo "Start of tests META"
+src/venv/bin/python3 src/main.py jsons/META.json
+echo "End of tests"
+echo "Start of tests NFLX"
+src/venv/bin/python3 src/main.py jsons/NFLX.json
+echo "End of tests"
+echo "Start of tests PKO"
+src/venv/bin/python3 src/main.py jsons/PKO.json
+echo "End of tests"
+echo "Start of tests TSLA"
+src/venv/bin/python3 src/main.py jsons/TSLA.json
 echo "End of tests"
 deactivate
