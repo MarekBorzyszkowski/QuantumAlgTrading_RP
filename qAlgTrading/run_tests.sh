537c9d2fd4bfb5389d9680b14635679697bb465f
marek <31738037+MarekBorzyszkowski@users.noreply.github.com>
2024-12-29 03:22:39 +0100

    setup before final run

--- qAlgTrading/run_tests.sh
+++ qAlgTrading/run_tests.sh
@@ -7,20 +7,18 @@ source src/venv/bin/activate
 
 echo "Start of tests S&P"
 src/venv/bin/python3 src/main.py jsons/S\&P.json
-#echo "Start of tests wig_20"
-#src/venv/bin/python3 src/main.py jsons/wig_20.json
-#echo "Start of tests META"
-#src/venv/bin/python3 src/main.py jsons/META.json
-#echo "Start of tests CDR"
-#src/venv/bin/python3 src/main.py jsons/CDR.json
+echo "Start of tests wig_20"
+src/venv/bin/python3 src/main.py jsons/wig_20.json
+echo "Start of tests NVDA"
+src/venv/bin/python3 src/main.py jsons/NVDA.json
+echo "Start of tests CDR"
+src/venv/bin/python3 src/main.py jsons/CDR.json
 #echo "Start of tests NFLX"
 #src/venv/bin/python3 src/main.py jsons/NFLX.json
 #echo "Start of tests PKO"
 #src/venv/bin/python3 src/main.py jsons/PKO.json
 #echo "Start of tests TSLA"
 #src/venv/bin/python3 src/main.py jsons/TSLA.json
-#echo "Start of tests NVDA"
-#src/venv/bin/python3 src/main.py jsons/NVDA.json
 #echo "Start of tests MSFT"
 #src/venv/bin/python3 src/main.py jsons/MSFT.json
 echo "End of tests"
